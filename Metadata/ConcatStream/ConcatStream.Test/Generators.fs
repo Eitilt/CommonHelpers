5108147fcf1ed2bccfd48df73cb942de3bb6fbbf
Eitilt <ag.eitilt@gmail.com>
2017-02-28 14:43:56 -0800

    Split ConcatStream into its own (future) submodule

---
+++ Metadata/ConcatStream/ConcatStream.Test/Generators.fs
@@ -0,0 +1,52 @@
+﻿module ConcatStream.Test.Generators
+
+open System.IO
+open FsCheck
+
+type ReadSeekWriteStream (bytes) =
+    inherit MemoryStream (bytes, true)
+    let buffer = bytes
+
+    override stream.ToString () =
+        sprintf "read, seek, write : %A" buffer
+
+
+    static member Generate =
+        Gen.apply
+        <| gen {
+                return fun bs -> new ReadSeekWriteStream (bs) :> Stream
+            }
+        <| Gen.sized (fun len -> Gen.arrayOfLength len Arb.generate<byte>)
+
+type ReadSeekStream (bytes) =
+    inherit MemoryStream (bytes, false)
+    let buffer = bytes
+
+    override stream.ToString () =
+        sprintf "read, seek, _____ : %A" buffer
+
+
+    static member Generate =
+        Gen.apply
+        <| gen {
+                return fun bs -> new ReadSeekStream (bs) :> Stream
+            }
+        <| Gen.sized (fun len -> Gen.arrayOfLength len Arb.generate<byte>)
+
+type Stream with
+    static member ArbInstance () =
+        [ ReadSeekWriteStream.Generate
+        ; ReadSeekStream.Generate
+        ]
+        |> Gen.oneof
+        |> Arb.fromGen
+Arb.register<Stream> () |> ignore
+
+type ReadableStream =
+    static member ArbInstance () =
+        [ ReadSeekWriteStream.Generate
+        ; ReadSeekStream.Generate
+        ]
+        |> Gen.oneof
+        |> Arb.fromGen
+Arb.register<ReadableStream> () |> ignore
---
+++ Metadata/ConcatStream/ConcatStream.Test/Program.fs
@@ -0,0 +1,13 @@
+﻿open FsCheck
+
+open ConcatStream.Test.Generators
+
+[<EntryPoint>]
+let main argv =
+    Gen.sample 5 5 Arb.generate<ReadableStream>
+    |> printfn "%A"
+    
+    printfn "Press any key to close..."
+    System.Console.ReadKey () |> ignore
+
+    0 // return an integer exit code
