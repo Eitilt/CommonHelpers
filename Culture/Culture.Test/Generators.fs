70cc3762a18e5f9b98ba92df03b1f0b87d26d7fe
Sam May <ag.eitilt@gmail.com>
2017-04-09 01:50:58 -0700

    Initial files for ISO 639-2 languages in .NETStandard

---
+++ Culture/Culture.Test/Generators.fs
@@ -0,0 +1,10 @@
+﻿(* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *)
+
+module AgEitilt.Common.Culture.Test.Generators
+
+open System
+open System.IO
+
+open FsCheck
---
+++ Culture/Culture.Test/Program.fs
@@ -0,0 +1,17 @@
+﻿(* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ *)
+
+open FsCheck
+
+open AgEitilt.Common.Stream.Test
+
+[<EntryPoint>]
+let main argv =
+    let culture = System.Globalization.CultureInfo "en"
+    System.Console.WriteLine culture.DisplayName
+    
+    printfn "Press any key to close..."
+    System.Console.ReadKey () |> ignore
+
+    0 // return an integer exit code
